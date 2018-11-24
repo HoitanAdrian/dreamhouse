ed173ecd8aa7a82d9b0a4e9de908184f1f4c2da2
Adrian Hoitan <adrian.hoitan@ricardo.ch>
2018-11-24 15:50:01 +0100

    Update application visuals
    - Update icon image
    - Add splash screen support
    - Change primary color

--- app/src/main/java/ch/dreamhouse/MainActivity.kt
+++ app/src/main/java/ch/dreamhouse/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Reset the theme to the default one from the launcher theme.
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
